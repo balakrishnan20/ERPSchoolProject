05a5775014148e9fd04cc2000b90fdb4a1b173e6
Yogesh <yogesh21218@gmail.com>
2023-08-02 22:10:23 +0530

    fourth commit

--- admin_rankcard.php
+++ admin_rankcard.php
@@ -1,10 +1,16 @@
 <?php
+include('config.php');
 include('header.php');
 include('sidenav.php');
+
 ?>
 
 
 
+<!-- <script type="text/javascript">
+     $('#form').submit(function (e)) {
+       e.preventDefault();
+     } -->
 
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -12,7 +18,7 @@ include('sidenav.php');
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
+            
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -26,58 +32,114 @@ include('sidenav.php');
 
     <!-- Main content -->
     <section class="content">
+  
+
 
       <!-- Default box -->
       <div class="card">
-        <div class="card-header bg-success ">
-          <h3 class="card-title">Rank Card</h3>
-</div>
+        <div class="card-header bg-success">
+         Marksheet
+        </div>
         <div class="card-body">
-          
-        <form action="db_adminrankcard.php" id="form" method="POST">
-          <div class="row">
-            <div class="col-sm-4">
-  <div class="form-group">
-    <label for="name">Exam Name:</label>
-    <input type="text" class="form-control"  name="Examname" id="email">
-  </div>
-</div>
+        <div class="row">
+    <div class="col-sm-4">
+      <label for="name">Student Name:</label>
+      <input type="text" class="form-control" id="studentname" placeholder="Enter name" name="StudentName">
+    </div>
+
+
+
+    <div class="col-sm-4">
+      <label for="name">Register Number:</label>
+      <input type="text" class="form-control" id="studentname" placeholder="Enter register number" name="RegisterNumber">
+    </div>
+
+
+
+    <div class="col-sm-4">
+      <label for="name">Class:</label>
+      <input type="text" class="form-control" id="studentname" placeholder="Enter class" name="Class">
+    </div>
+    </div>
+<br>
+   
+<div class="row">
+    <div class="col-sm-4">
+      <label for="name">Section:</label>
+      <input type="text" class="form-control" id="studentname" placeholder="Enter section" name="Section">
+    </div>
   
-<div class="col-sm-4">
-  <div class="form-group">
-    <label for="name">Class:</label>
-    <input type="text" class="form-control"  name="Class" id="email">
-  </div>
-</div>
 
+    <div class="col-sm-4">
+        <label for="name">Subject Name:</label>
+        <select name="SubjectName"  class="form-control">
+        <?php
+          $ql = "SELECT * FROM add_subj";
+          $result = mysqli_query($conn,$ql);
   
+          if (mysqli_num_rows($result) > 0)  {
+            while ($row = mysqli_fetch_assoc($result)) {
+              // echo "<option value="$row['SubjectName']">Select</option>";
+    
+              echo "<option value='" . $row['SubjectName'] . "'>".$row['SubjectName'] ."</option>";
+            }
+          }
+          else {
+            echo "<option value=''>No subjects found</option>";
+        }
+              
 
-<div class="col-sm-4">
-  <div class="form-group">
-    <label for="name">Section:</label>
-    <input type="text" class="form-control" name="Section" id="email">
-  </div>
+        ?>
+            </select>
+    </div>
+  
+    <br>
+    
+    <div class="col-sm-4">
+        <label for="name">Subject Mark:</label>
+      <input type="text" class="form-control" id="studentname" placeholder="Enter subject code" name="SubjectMark">
+    </div>
+      </div>
+
+
+<br>
+
+
+
+<div class="row">
+    <div class="col-sm-4">
+      <label for="name">Total Marks:</label>
+      <input type="text" class="form-control" id="studentname" placeholder="Enter total marks" name="Total Marks">
+    </div>
+
+
+    <div class="col-sm-4">
+      <label for="name">Grade:</label>
+      <input type="text" class="form-control" id="studentname" placeholder="Enter Grade" name="Grade">
+    </div>
+
+    <div class="col-sm-4">
+      <label for="name">Result:</label>
+      <input type="text" class="form-control" id="studentname" placeholder="Enter result" name="result">
+    </div>
 </div>
 
-  <br>
-  <button type="submit" id="div1" name="submit" class="btn btn-success">Submit</button>
+<br>
+<button type="submit" id="div1" name="submit" class="btn btn-success">Submit</button>
 </form>
-
+      </div>
         </div>
         <!-- /.card-body -->
-       
+        
         <!-- /.card-footer-->
-      </div>
+      
       <!-- /.card -->
 
-    </section>
+    
     <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->                
+  <!-- /.content-wrapper -->
 <?php
 include('footer.php');
 include('script.php');
-
 ?>
-
-
